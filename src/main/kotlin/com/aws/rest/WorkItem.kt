11038872d54424c425d2bf36b8c177a0bad7f46b
ASALAZAR919 <a.salazar@moabits.com>
2022-11-23 00:10:27 -0500

    st

---
+++ src/main/kotlin/com/aws/rest/WorkItem.kt
@@ -0,0 +1,13 @@
+package com.aws.rest
+//package com.example.demo
+
+class WorkItem {
+
+    var id: String? = null
+    var arc: String? = null
+    var name: String? = null
+    var guide: String? = null
+    var date: String? = null
+    var description: String? = null
+    var status: String? = null
+}
